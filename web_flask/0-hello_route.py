cc22e1b261f73945a4bde5024b9b9c9970ffcc85
muluante <muluante@outlook.com>
2023-08-17 11:24:59 +0000

    new commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 app = Flask("__name__")
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_hbnb():
     """ Prints a Message when / is called """
     return render_template("10-hbnb_filters.html")

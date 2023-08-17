92ba41016ca5dfad84d0c4e50ea2fe4c025fe871
muluante <muluante@outlook.com>
2023-08-17 11:31:31 +0000

    new co

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
-from flask import Flask, render_template
+from flask import Flask
 app = Flask("__name__")
 
 
 @app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_hbnb():
     """ Prints a Message when / is called """
-    return render_template("10-hbnb_filters.html")
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     """ Main Function """
